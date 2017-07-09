63ae8df99c0a890ee46470a2ea73a38eba927d62
Jeremiah Flickinger <jeremiahflickinger@Jeremiahs-MacBook-Air.local>
2017-07-10 09:32:41 +1000

    added a sample app

---
+++ nenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jeremiahflickinger/Desktop/corona_sdk_django_web_api/nenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
